72bfcdd4d2d9eca11b89e0ddcdc69dfca47c2301
Melvin Carvalho <melvincarvalho@gmail.com>
2020-05-18 15:42:29 +0200

    add apps

---
+++ app/reddit.js
@@ -0,0 +1,111 @@
+import { html, render, Component } from '/web_modules/spux.js'
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      items: [],
+      sub: new URLSearchParams(window.location.search).get('r') || 'EarthPorn'
+    }
+  }
+
+  updateStories (items) {
+    this.setState({ items: items })
+  }
+
+  componentDidMount () {
+    let limit = 100
+    var items = []
+    const that = this
+
+    fetch(
+      'https://www.reddit.com/r/' + this.state.sub + '/.json?limit=' + limit
+    )
+      .then(function (response) {
+        if (response.status !== 200) {
+          console.log(
+            'Looks like there was a problem. Status Code: ' + response.status
+          )
+          return
+        }
+
+        response.json().then(function (data) {
+          var children = data.data.children
+
+          children.forEach((k, v) => {
+            let title = k.data.title
+            let url = k.data.url
+            let thumbnail = k.data.thumbnail
+            items.push({ title: title, url: url, thumbnail: thumbnail })
+          })
+          that.updateStories(items)
+        })
+      })
+      .catch(function (err) {
+        console.log('Fetch Error :-S', err)
+      })
+  }
+
+  render () {
+    const Img = props => {
+      if (props.src && props.src !== 'self') {
+        return html`
+          <img src="${props.src}" />
+        `
+      } else {
+        return
+      }
+    }
+
+    const RedditComponent = props => {
+      let col = props.item % 5 == 4 ? '' : '<DIV />'
+      if (props.item % 5 === 4) {
+        return html`
+          <div class="card col">
+            <${Img} src="${props.thumbnail}" />
+            <h4>${props.title}</h4>
+            <p>
+              <a href="${props.url}" target="_blank">Click For Full Story</a>
+            </p>
+          </div>
+          <hr />
+        `
+      } else {
+        return html`
+          <div class="card col">
+            <${Img} src="${props.thumbnail}" />
+            <h4>${props.title}</h4>
+            <p>
+              <a href="${props.url}" target="_blank">Click For Full Story</a>
+            </p>
+          </div>
+        `
+      }
+    }
+
+    return html`
+      <h2>Reddit ${this.state.sub}</h2>
+      <hr />
+
+      <div>
+        ${this.state.items.map((item, i) => {
+          return html`
+            <${RedditComponent}
+              title="${item.title}"
+              url="${item.url}"
+              thumbnail="${item.thumbnail}"
+              item="${i}"
+            />
+          `
+        })}
+      </div>
+    `
+  }
+}
+
+render(
+  html`
+    <${App} />
+  `,
+  document.body
+)
