a6cfac426fa52c40ab109d24bfc72f592b445059
Melvin Carvalho <melvincarvalho@gmail.com>
2020-05-25 17:53:03 +0200

    make path absolute

--- shim.js
+++ shim.js
@@ -3,10 +3,6 @@ console.log('window.location.href', window.location.href)
 
 let shimScript =
   './' +
-  window.location.href
-    .split('/')
-    .pop()
-    .replace('.html', '')
-    .concat('.js')
+  window.location.href.replace('.html', '').concat('.js')
 
 import(shimScript)
